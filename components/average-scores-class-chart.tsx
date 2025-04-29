083ffc81e492b54203ee1c7bded7633a21aa20b3
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2025-04-29 07:33:06 +0700

    fix: typo

--- components/average-scores-class-chart.tsx
+++ components/average-scores-class-chart.tsx
@@ -90,10 +90,10 @@ export function AverageScoresClassChart() {
       </CardContent>
       <CardFooter className="flex-col items-start gap-2 text-sm">
         <div className="flex gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+          Data menurun sebanyak 5.2% pada bulan ke 5 <TrendingUp className="h-4 w-4" />
         </div>
         <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
+          Menampilkan data rata-rata nilai dari 6 bulan terakhir
         </div>
       </CardFooter>
     </Card>
